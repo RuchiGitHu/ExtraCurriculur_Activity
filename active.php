fa3c07b163426e1d06c17e863df4d4645a7d2e22
RuchiGitHu <sahu2101ruchi@gmail.com>
2020-12-29 09:02:04 -0700

    about.php third commit added

---
+++ active.php
@@ -0,0 +1,393 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ 
+</head>
+<body>
+<style>
+html{
+  scroll-behavior:smooth;
+}
+*{ margin: 0; padding: 0; box-sizing: border-box; font-family: 'Source Serif Pro', serif;}
+a{ text-decoration: none;}
+a:hover{
+  text-decoration: none;
+}
+li {
+list-style:none;
+}
+p{
+  
+  font-size: 0.9rem;
+  line-height: 1.6;
+  font-weight: 400;
+  color: #606060;
+  
+}
+.edit h1{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 2.5rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+}
+.edit h2{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+margin-top: 10px;
+}
+.edit li{
+  margin-bottom: 15px;
+font-size: 1rem!important;
+font-weight:600;
+margin-left: 150px;
+
+}
+.edit p{
+  color:#330500;
+  text-align:left;
+ /* width:1200px;*/
+  margin-left: 40px;
+  margin-right: 50px;
+
+}
+.header-section{
+  width: 100%;
+  height: 100vh;
+  background-image:url(../images/h.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+/*     clip-path: polygon(100% 0%,100% 75%, 50% 100%, 0 75%, 0 0%); 
+*/       position: relative;
+}
+.navbar{
+  background-color: LightSkyBlue;
+}
+.nav-item a{
+  color:#000000!important;
+  font-weight: bold;
+}
+.nav-item{
+ /* border: 1px solid #fff;*/
+  border-radius:100px;
+  margin: 0.5px;
+  padding: 12px, 35px;
+  outline: none;
+  color: #fff;
+  font-size:1rem;
+  font-weight: 400;
+  line-weight:1.4;
+  text-align: center;
+}
+.navbar-brand{
+   color:#000000!important;
+  font-weight: bold;
+}
+.header-section{
+  width:100%;
+  height:50%;
+  
+  color: black;
+  text-align: center;
+  position: relative;
+}
+.center-div{
+  width:100%;
+  height:auto;
+  position: center;
+  color: black;
+  top: 50%;
+  left: 50%;
+  font-family: 'Dancing Script', cursive;
+
+}
+
+.footersection{
+  width: 100%;
+  height: auto;
+  padding: 70px 0 20px 0;
+  background: #00abff;
+  position:relative;
+
+}
+.footersection p{
+  color: #fff;
+text-align: center;
+}
+.footersection li a{
+  font-size:0.9rem;
+  line-height:1.6;
+  font-weight: 400;
+  color:#fff;
+  text-transform: capitalize;
+
+}
+.footersection h3{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 1.2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+}
+#myBtn {
+  display: none;
+  position: fixed;
+bottom: 20px;
+ right: 40px;
+z-index: 99;
+border: none;
+color: black;
+background: #00abff;
+padding:10px;
+border-radius: 10px;
+}
+#myBtn:hover{
+  background: white;
+
+}
+.footer-div li{
+  text-align: left;
+}
+.container1{
+  text-align: center;
+/*background-color: PaleTurquoise;*/
+font-color: black;
+height:250px;
+width: 1200px;
+left:10px;
+
+ margin-left: 40px;
+}
+.container1:hover{
+   box-shadow: 0 0 20px 0 rgba(0,0,0,0.3);
+transform: translateY(-10px);
+
+}
+.ruh:before{
+  content:"";
+  position: absolute;
+  top:80px;
+  left:0;
+  bottom:0;
+  right:10px;
+  background: linear-gradient(340deg, LightSkyBlue 55%, #fff 0%);
+  z-index:-1;
+  height: 1520px;
+
+}
+
+</style>
+<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#"><h4 class="font-weight-bold" >ACTIVITIES</h4></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="benefits.php">BENEFITS</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="photo.php">PHOTO-GALLERY</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="about.php">ABOUT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<section class= "header-section">
+<div class="center-div">
+</div>
+</section>
+
+<div class="edit">
+<div class="ruh">
+
+
+<h2 class="font-weight-bold">DANCING</h2>
+<p>"Stars are always dancing. Sometimes they dance twinkling away with the rhythm of your joyful heart and sometimes they dance without movement to embrace your heartache as if frozen sculptures of open-armed sadness".  Sometimes in life confusion tends to arise and only dialogue of dance seems to make sense. Dance is a timeless interpretation of life.Don’t practice until you get it right; practice until you can’t get it wrong.
+If you opened the dictionary and searched for the meaning of a Goddess, you would find the reflection of a dancing lady. Dance like no one is watching – because they’re not, they’re checking their phones. without saying a word you express your emotions through the dance.</p>
+<div class="container1">
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/ab.png" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/bc.png" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/cd.png" width="400" height="250">
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+<h2 class="font-weight-bold">ACTING</h2>
+<p>"Children more than ever, need opportunities to be in their bodies in the world – jumping rope, bicycling, stream hopping, and fort building. It’s this engagement between limbs of the body and bones of the earth where true balance and centeredness emerge.
+
+"Play is not a break from learning. It is endless, delightful, deep, engaging, practical learning. It’s the doorway into the child’s heart!"Life is playfulness…We need to play so that we can rediscover the magic all around us.The activities that are the easiest, cheapest, and most fun to do – such as singing, playing games, reading, storytelling, and just talking and listening – are also the best for child development.</p>
+<div class="container1">
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/x.png" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/y.png" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/a.png" width="400" height="250">
+</div>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+<h2 class="font-weight-bold">EVENT ACTIVITY</h2>
+<p>"The word ‘education’ comes from the Latin ‘educere’ = e- (out of) + -ducere (to draw). Education is not just about putting information in. We have forgotten that it, in fact, begins in the child’s heart.By education I mean an all-round drawing out of the best in the child and man; body, mind and spirit."
+We worry about what a child will become tomorrow, yet we forget that he is someone today.Kids don’t remember what you try to teach them. They remember what you are.The work of education is divided between the teacher and the environment"</p>
+<div class="container1">
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/j.jpeg" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/e.jpg" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/l.jpeg" width="400" height="250">
+</div>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+
+<h2 class="font-weight-bold">FASTIVE ACTIVITY</h2>
+<p>"Live every day as if it is a festival. Turn your life into a celebration."The greatness of a culture can be found in its festivals. A national festival is an occasion to refine and rebuild the national character. 
+Festivals promote diversity, they bring neighbors into dialogue, they increase creativity, they offer opportunities for civic pride, they improve our general psychological well-being. In short, they make cities better places to live.   
+Each year, every city in the world that can should have a multiday festival. More people meeting each other, digging new types of music, new foods, new ideas. You want to stop having so many wars? This could be a step in the right direction."</p>
+<div class="container1">
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/i.jpeg" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/p.jpeg" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-4 col-md-4 col-10">
+<div>
+<img src="../images/o.jpeg" width="400" height="250">
+</div>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+<footer class="footersection" id="footerdiv">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+<div>
+<h3> ABOUT SCHOOL</h3>
+<p>The vision of the school is to become a Centre of Excellence and to create global citizens and visionary leaders who function as catalysts in different spheres of life and this could be achieved by drawing the best from each student by providing an enabling and caring environment.</p>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+<div>
+<h3>NAVIGATION LINK</h3>
+<li><a href="index.php">HOME</a></li>
+<li><a href="#">SERVICES</a></li>
+<li><a href="benefits.php">BANEFITS</a></li>
+<li><a href="about.php">ABOUT</a>
+</li>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-12 col-15 footer-div text-center">
+<div>
+<h3> NEWS LETTER</h3>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Pellentesque nulla ipsum, pretium ac ultrices non, tincidunt id quam. Mauris dignissim dolor quis risus molestie et ,</p>
+</div>
+</div>
+</div>
+<div class="mt-5 text-center">
+<p>Copyright 02019 ALL rights reserved | This template is made for my PROJECT</p>
+</div>
+</div>
+</footer>
+
+
+
+</body>
+</html>
