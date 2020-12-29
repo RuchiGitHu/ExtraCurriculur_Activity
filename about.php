b605c99f551d8c25fc9732f38d55856acbe07656
RuchiGitHu <sahu2101ruchi@gmail.com>
2020-12-29 15:10:41 -0700

    about.php four commit

---
+++ about.php
@@ -0,0 +1,309 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ 
+</head>
+<body>
+<style>
+html{
+  scroll-behavior:smooth;
+}
+*{ margin: 0; padding: 0; box-sizing: border-box; font-family: 'Source Serif Pro', serif;}
+a{ text-decoration: none;}
+a:hover{
+  text-decoration: none;
+}
+li {
+list-style:none;
+}
+p{
+  
+  font-size: 0.9rem;
+  line-height: 1.6;
+  font-weight: 400;
+  color: #606060;
+
+}
+.edit h1{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 2.5rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+}
+.edit h2{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+}
+.edit li{
+  margin-bottom: 15px;
+font-size: 1rem!important;
+font-weight:600;
+margin-left: 150px;
+
+}
+.edit p{
+  color:#330500;
+  text-align:left;
+ /* width:1200px;*/
+  margin-left: 40px;
+  margin-right: 50px;
+}
+.header-section{
+  width: 100%;
+  height: 100vh;
+  background-image:url(../images/v.jpg);
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+/*     clip-path: polygon(100% 0%,100% 75%, 50% 100%, 0 75%, 0 0%); 
+*/       position: relative;
+}
+.navbar{
+  background-color: LightSkyBlue;
+}
+.nav-item a{
+  color:#000000!important;
+  font-weight: bold;
+}
+.nav-item{
+ /* border: 1px solid #fff;*/
+  border-radius:100px;
+  margin: 0.5px;
+  padding: 12px, 35px;
+  outline: none;
+  color: #fff;
+  font-size:1rem;
+  font-weight: 400;
+  line-weight:1.4;
+  text-align: center;
+}
+.navbar-brand{
+   color:#000000!important;
+  font-weight: bold;
+}
+.header-section{
+  width:100%;
+  height:50%;
+  
+  color: black;
+  text-align: center;
+  position: relative;
+}
+.center-div{
+  width:100%;
+  height:auto;
+  position: center;
+  color: black;
+  top: 50%;
+  left: 50%;
+  font-family: 'Dancing Script', cursive;
+
+}
+
+.footersection{
+  width: 100%;
+  height: auto;
+  padding: 70px 0 20px 0;
+  background: #00abff;
+  position:relative;
+
+}
+.footersection p{
+  color: #fff;
+
+}
+.footersection li a{
+  font-size:0.9rem;
+  line-height:1.6;
+  font-weight: 400;
+  color:#fff;
+  text-transform: capitalize;
+
+}
+.footersection h3{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 1.2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+}
+#myBtn {
+  display: none;
+  position: fixed;
+bottom: 20px;
+ right: 40px;
+z-index: 99;
+border: none;
+color: black;
+background: #00abff;
+padding:10px;
+border-radius: 10px;
+}
+#myBtn:hover{
+  background: white;
+
+}
+.container1{
+  text-align: center;
+/*background-color: PaleTurquoise;*/
+font-color: black;
+height:250px;
+width: 1200px;
+left:10px;
+
+ margin-left: 40px;
+}
+.container1:hover{
+   box-shadow: 0 0 20px 0 rgba(0,0,0,0.3);
+transform: translateY(-10px);
+
+}
+.ruh:before{
+  content:"";
+  position: absolute;
+  top:80px;
+  left:0;
+  bottom:0;
+  right:10px;
+  background: linear-gradient(330deg, LightSkyBlue 55%, #fff 0%);
+  z-index:-1;
+  height: 1520px;
+
+}
+
+</style>
+<nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#">ST.AUGUSTINE CO-ED SCHOOL</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+      </li>
+
+      
+
+       <li class="nav-item">
+        <a class="nav-link" href="active.php">ACTIVITIES</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="benefits.php">BENEFITS</a>
+      </li>
+
+<li class="nav-item">
+        <a class="nav-link" href="photo.php">PHOTO-GALLERY</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+<section class= "header-section">
+<div class="center-div">
+</div>
+</section>
+
+<div class="edit">
+<h1 class="font-weight-bold" >ABOUT US</h1>
+<div class="ruh">
+<p>"The joy and instinctive sense of wonder and delight that play brings validates it as the most central, natural, and important aspect of childhood. Believing in a play-based philosophy is simply accepting that children naturally engage in and enjoy play as a means of learning on their terms, in their own way, and at their own pace. It is my great pleasure to welcome you all to sunny Group Of institutions, that does not confer degree along, but creates thinkers & dreamers of tomorrow . We not only teach subjects, We create inquisitiveness amongst your minds, which remains a lifelong asset in our students. We inculcate an attitude amongst our students......</p>
+<p>We are committed to providing rich experiences to promote cognitive, social, and emotional development and ensure students have a solid foundation needed for lifelong learning. With a student-centered approach, students are active participants in their own learning. Activities are designed to allow students to explore and discover. acknowledges children as strong, capable, resilient, and full of wonder and knowledge. Every child brings with him or her a deep curiosity and potential. We combine an appreciation of beauty and multiculturalism with a disciplined academic program rooted in math and science. </p>
+
+<h2 class="font-weight-bold">PRINCIPAL MASSAGE</h2>
+<div class="container1">
+<div class="row">
+
+<div class="col-lg-4 col-md-4 col-10   footer-div">
+<div>
+<img src="../images/s.jpeg" width="400" height="250">
+</div>
+</div>
+
+<div class="col-lg-8 col-md-5  footer-div">
+<div>
+<p>"Our mission is to Educate and Train students to enable them to face the Global competition and to install high patterns of Discipline & Ethics with the help involved management and Dedicated Faculty members" Sports and Extra Curricular Activities: Study and sports are two linked components of our learning process and will always be important for an individual. A healthy body.it is rightly said that a healthy nation can be built if most of the citizens are physically fit. It is my great pleasure to welcome you all to sunny Group Of institutions, that does not confer degree along, but creates thinkers & dreamers of tomorrow . We not only teach subjects, We create inquisitiveness amongst your minds, which remains a lifelong asset in our students. We inculcate an attitude amongst our students. The sense of community between teachers, parents, and children creates a nurturing, caring space for young minds to grow, tender hearts to open and an inspired life to begin</p>
+</div>
+</div>
+
+
+</div></div>
+
+
+
+<h2 class="font-weight-bold" >Core Values</h2>
+<p>Our individualized learning plans, small class sizes, one-on-one instruction, and a developmentally-appropriate curriculum founded in brain research create a safe space for our students to explore their interests, believe in their dreams, and ignite their imaginations through play. Our goal is to help children become trusting, independent thinkers who display self-knowledge, respect, and empathy for other people. In early childhood, play is the best way to harness the learning power of children, stimulate creativity, and heighten retention.</p>
+<li> * Strengthens student motivation</li>
+<li> * Promotes peer communication</li>
+<li> * Reduces disruptive behavior</li>
+<li> * Builds student-teacher relationships</li>
+<li> * Supports discovery/active learning</li>
+<li> * Encourages responsibility for one’s own learning</li>
+<p>Our core values Curiosity, Creativity, and Community are woven into every facet of our curriculum and fostered by every member of our school. Our hands-on, experiential learning and engaging approach promotes individual growth and achievement while also emphasizing social responsibility and good citizenship.</p>
+<h2 class="font-weight-bold">OUR MISSION </h2>
+<p>The Seven Arrows Way is based on the profound pedagogy and philosophy of Experiential Learning. We ground our educational approach in leading-edge brain research. The Theory of Multiple Intelligences, developed at Harvard University by cognitive scientist Dr. Howard Gardner, holds that different people have diverse ways of learning, remembering, performing, processing, and understanding information. Gardner posits that there are seven distinct intelligences: Visual-Spatial, Bodily-Kinesthetic, Musical, Interpersonal, Intrapersonal, Linguistic, and Logical-Mathematical. Our curriculum is designed to explore these intelligences, and to identify each child’s individual learning style, in order to optimize his or her educational results.
+
+We also draw upon Dr. Richard Davidson’s research in affective neuroscience, which focuses on how a child’s emotional state affects the way he or she absorbs information. 
+</p>
+
+
+</div>
+
+</div>
+
+
+
+
+<footer class="footersection" id="footerdiv">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+<div>
+<h3> ABOUT SCHOOL</h3>
+<p>The vision of the school is to become a Centre of Excellence and to create global citizens and visionary leaders who function as catalysts in different spheres of life and this could be achieved by drawing the best from each student by providing an enabling and caring environment.</p>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+<div>
+<h3>NAVIGATION LINK</h3>
+<li><a href="index.php">HOME</a></li>
+<li><a href="#">SERVICES</a></li>
+<li><a href="benefits.php">BANEFITS</a></li>
+<li><a href="about.php">ABOUT</a>
+</li>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-12 col-15 footer-div text-center">
+<div>
+<h3> NEWS LETTER</h3>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Pellentesque nulla ipsum, pretium ac ultrices non, tincidunt id quam. Mauris dignissim dolor quis risus molestie et ,</p>
+</div>
+</div>
+</div>
+<div class="mt-5 text-center">
+<p>Copyright 02019 ALL rights reserved | This template is made for my PROJECT</p>
+</div>
+<div class="scrolltop float-right">
+<i class="fa fa-arrow-up" onclick="topfunction()" id="myBtn"></i>
+
+</div>
+</div>
+</footer>
+
+
+
+</body>
+</html>
