ca6055e6dd45062a6a720ccfe1f72b1530375cb4
RuchiGitHu <sahu2101ruchi@gmail.com>
2020-12-29 09:39:50 -0700

    benefits.php five commit added

---
+++ benefits.php
@@ -0,0 +1,297 @@
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ 
+</head>
+<body>
+<style>
+html{
+  scroll-behavior:smooth;
+}
+*{ margin: 0; padding: 0; box-sizing: border-box; font-family: 'Source Serif Pro', serif;}
+a{ text-decoration: none;}
+a:hover{
+  text-decoration: none;
+}
+li {
+list-style:none;
+}
+.about-section em{
+	font-color: white;
+}
+
+p{
+	left: 5px;
+	right: 5px
+  font-size: 0.9rem;
+  line-height: 1.6;
+  font-weight: 400;
+  color: black;
+  text-align:center
+}
+.navbar{
+  background-color:LightSlateGray;
+}
+.mine{
+	top: 0;
+	background-image:url("../images/be.jpg");
+}
+
+.nav-item a{
+  color:#000000!important;
+  font-weight: bold;
+}
+.nav-item{
+ /* border: 1px solid #fff;*/
+  border-radius:100px;
+  margin: 0.5px;
+  padding: 12px, 35px;
+  outline: none;
+  color: #fff;
+  font-size:1rem;
+  font-weight: 400;
+  line-weight:1.4;
+  text-align: center;
+}
+.navbar-brand{
+   color:#000000!important;
+  font-weight: bold;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.about-section {
+  padding: 50px;
+  text-align: center;
+  background-color: #474e5d;
+  color: white;
+ 
+}
+
+.container {
+  padding: 0 16px;
+}
+
+.container::after{
+  content: "";
+  clear: both;
+  display: table;
+}
+.edit h2{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+margin-top: 10px;
+}
+.edit li{
+  margin-bottom: 15px;
+font-size: 1rem!important;
+font-weight:600;
+margin-left: 150px;
+
+}
+.edit p{
+  color:#330500;
+  text-align:left;
+ /* width:1200px;*/
+  margin-left: 40px;
+  margin-right: 50px;
+
+}
+
+
+.edit h1{
+	text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 1.2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+text-align: center;
+}
+.footersection{
+  width: 100%;
+  height: auto;
+  padding: 70px 0 20px 0;
+  background: #00abff;
+  position:relative;
+
+}
+.footersection p{
+  color: #fff;
+    text-align: center;
+
+}
+.footersection li a{
+  font-size:0.9rem;
+  line-height:1.6;
+  font-weight: 400;
+  color:#fff;
+  text-transform: capitalize;
+
+}
+.footersection h3{
+  text-transform: uppercase;
+  color: black;
+  margin-bottom: 25px;
+font-size: 1.2rem!important;
+font-weight:600;
+text-shadow:0 2px 5px rgba(0,0,0,0.3);
+}
+.ruh{
+  background: LightSkyBlue;
+  margin-top:10px;
+}
+@media(max-width: 768px){
+   .nav-item{
+    text-align:center!important;
+  }
+ 
+.footer-div:nth-child(2){
+text-align:left!important;
+}
+.footer-div{
+  margin: 30px 0;
+}
+}
+
+
+</style>
+<nav class="navbar navbar-expand-lg bg-pink">
+  <a class="navbar-brand">ST.AUGUSTINE CO-ED SCHOOL</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">HOME <span class="sr-only">(current)</span></a>
+      </li>
+
+      
+
+      <li class="nav-item">
+        <a class="nav-link" href="active.php">ACTIVITIES</a>
+      </li>
+
+<li class="nav-item">
+        <a class="nav-link" href="photo.php">PHOTO-GALLERY</a>
+      </li>
+<li class="nav-item">
+        <a class="nav-link" href="about.php">ABOUT</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<div class="mine">
+<img  width="1260" height="500">
+</div>
+
+<div class="about-section">
+  <h1>BENEFITS OF SCHOOL ACTIVITIES</h1>
+  <p><em>Some text about who we are and what we do.The vision of the school is to become a Centre of Excellence and to create global citizens and visionary leaders who function as catalysts in different spheres of life and this could be achieved by drawing the best from each student by providing an enabling and caring environment.</em></p>
+</div>
+<h1></h1>
+<p></p>
+<div class="edit">
+<div class="ruh">
+
+ <h1>The Definition of Extracurricular Activities</h1>
+<p>Before we get into the nitty gritty, let’s take a deeper look at the definition of extracurricular activities.
+
+The word extracurricular is a combination of the prefix extra, which translates to “on the outside” and curriculum, which translates to “a running course/career.”
+However, simply playing soccer with your friends on the weekends for fun isn’t actually an extracurricular activity, even though it has nothing to do with school.
+
+Extracurricular activities require a regular time commitment and initiative such as being on a sports team, forming a student newspaper, playing the violin in the local orchestra or taking an online course on robotics.
+<h1>The Benefits of Extracurricular Activities</h1>
+
+<h1>1. Improved Academic Performance</h1>
+<p>Some students worry that participating in extracurriculars may take away too much time from their schoolwork, thus hurting their grades; however, extracurricular activities can actually improve your grades and your outlook on school in general!</p>
+<p>
+Participating in activities you are passionate about can increase your brain function, help you concentrate and manage your time better, all of which contribute to higher grades. High endurance sports, for example, will train you to focus and build stamina in the face of intense difficulty. This gives you an advantage when it comes to studying and taking exams.</p>
+
+<h1>2. Explore Interests and Create Broader Perspectives</h1>
+<p>When you participate in multiple different activities, you’ll get the opportunity to explore a range of interests and unlock passions you never knew you had!
+
+Plus, diversifying your interests subsequently broadens your world view.
+
+Think about it this way: if you join a philosophy club you’ll begin to look at the world through the eyes of a budding philosopher.</p>
+
+<h1>3. Higher Self-esteem</h1>
+<p>The more you achieve success through activities you’re passionate about, the more your self confidence will improve.
+
+For example, let’s say you’re really good at maths and your teacher encourages you to get involved in competitions. You join the school team and start training for the national Maths Olympiad. During the process you realise how fun maths can be and how talented you actually are, which gives your confidence a massive boost.
+
+Working hard and mastering new skills in a fun, relaxed – and sometimes competitive – setting allows you to be successful without the pressure of getting a good grade.
+
+Plus, once your confidence improves, you’ll be more open to taking risks in all aspects of your life, not just in Maths Olympiads.</p>
+<h1>4. Essential Life Skills</h1>
+<p>On top of all of the benefits of extracurricular activities we already talked about, one of the greatest advantages extracurricular activities give you are “real world” skills.
+<p>The more you push yourself in your extracurricular endeavours, the more you’ll develop these skills. If you’re passionate about coding, you might join the school coding club, where you’ll develop teamwork, problem solving and analytical thinking skills.
+
+But you could take that passion even further and create your own coding club, where you’ll develop goal setting, time management, prioritisation, leadership and public speaking skills.
+
+Phew! That’s a lot of responsibility. Each and every step will teach you a new skill you can use for the rest of your life.</p>
+</div></div>
+
+<footer class="footersection" id="footerdiv">
+<div class="container">
+<div class="row">
+
+
+<div class="col-lg-4 col-md-6 col-12 footer-div text-center">
+<div>
+<h3> ABOUT SCHOOL</h3>
+<p>The vision of the school is to become a Centre of Excellence and to create global citizens and visionary leaders who function as catalysts in different spheres of life and this could be achieved by drawing the best from each student by providing an enabling and caring environment.</p>
+</div>
+</div>
+
+<div class="col-lg-3 col-md-6 col-12 footer-div text-center">
+<div>
+<h3>NAVIGATION LINK</h3>
+<li><a href="index.php">HOME</a></li>
+<li><a href="#">SERVICES</a></li>
+<li><a href="about.php">ABOUT</a>
+</li>
+</div>
+</div>
+
+<div class="col-lg-4 col-md-12 col-12 footer-div text-center text-align-left">
+<div>
+<h3> NEWS LETTER</h3>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. Pellentesque nulla ipsum, pretium ac ultrices non, tincidunt id quam. Mauris dignissim dolor quis risus molestie et ,</p>
+</div>
+</div>
+
+<div class="mt-5 text-center">
+<p>Copyright 02019 ALL rights reserved | This template is made for my PROJECT</p>
+</div>
+
+
+</div>
+</div>
+</footer>
+
+
+</body>
+</html>
